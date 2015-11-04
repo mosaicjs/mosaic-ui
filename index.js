507c612fe7b61358c64da61cf1344638081b2c5b
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-11-04 10:17:11 +0100

    Add a popup panel and progress bar UI widgets

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import DataSetLayout from './lib/DataSetLayout';
 import DialogBox from './lib/DialogBox';
+import PopupPanel from './lib/PopupPanel';
+import ProgressBar from './lib/ProgressBar';
 import Utils from './lib/Utils';
 import View from './lib/View';
 import ViewLayout from './lib/ViewLayout';
@@ -8,6 +10,8 @@ export default {
     DataSetLayout,
     DialogBox,
     Utils,
+    PopupPanel,
+    ProgressBar,
     View,
     ViewLayout
 }
